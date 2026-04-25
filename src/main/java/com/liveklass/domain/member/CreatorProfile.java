5b2baa839d6d9cdc866277aafe4510d24528a7d4
sunjong0214 <rlatjswhd8200@gmail.com>
2026-04-25 18:02:24 +0900

    feat: Add creator profile validation

--- src/main/java/com/liveklass/domain/member/CreatorProfile.java
+++ src/main/java/com/liveklass/domain/member/CreatorProfile.java
@@ -1,6 +1,8 @@
 package com.liveklass.domain.member;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +14,16 @@ public class CreatorProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "creator_profile_id")
     private Long id;
 
+    @NotNull
     @Column(nullable = false, unique = true)
     private Long memberId;
 
+    @NotBlank
+    @Lob
+    @Column(nullable = false, columnDefinition = "BLOB")
     private String bio;
 
     public CreatorProfile(final Long memberId, final String bio) {
